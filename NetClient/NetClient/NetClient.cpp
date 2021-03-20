157858c6d8ed4d15263c273eb44649470cf9c80e
AndrewBorisov81 <ibobojohn81@gmail.com>
2021-03-20 11:52:56 +0200

    Changed NetClient.cpp

--- NetClient/NetClient/NetClient.cpp
+++ NetClient/NetClient/NetClient.cpp
@@ -6,9 +6,36 @@
 //
 
 #include <iostream>
+#include <olc_net.h>
+
+enum class CustomMsgTypes : uint32_t
+{
+  FireBullet,
+  MovePlayer
+};
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    olc::net::message<CustomMsgTypes> msg;
+    msg.header.id = CustomMsgTypes::FireBullet;
+    
+    int a = 1;
+    bool b = true;
+    float c = 3.14159f;
+    
+    struct
+    {
+        float x;
+        float y;
+    } d[5];
+    
+    msg << a << b << c << d;
+    
+    a = 99;
+    b = false;
+    c = 99.0f;
+    
+    msg >> d >> c >> b >> a;
+    
+    
     return 0;
 }
