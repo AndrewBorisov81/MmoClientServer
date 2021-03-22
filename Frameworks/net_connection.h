091998d20f5ae10f6bbaf76ef620770630903bb3
AndrewBorisov81 <ibobojohn81@gmail.com>
2021-03-22 13:25:02 +0200

    Update net_connection

--- Frameworks/net_connection.h
+++ Frameworks/net_connection.h
@@ -1,12 +1,46 @@
-//
-//  net_connection.h
-//  MmoClientServer
-//
-//  Created by Andrew Borisov on 20.03.2021.
-//
+#pragma once
 
-#ifndef net_connection_h
-#define net_connection_h
+#include "net_common.h"
+#include "net_tsqueue.h"
+#include "net_message.h"
 
-
-#endif /* net_connection_h */
+namespace olc
+{
+  namespace net
+  {
+    template<typename T>
+    class connection : public std::enable_shared_from_this<connection<T>>
+    {
+    public:
+      connection()
+      {}
+        
+      virtual ~connection()
+      {}
+        
+      public:
+        bool ConnectToServer();
+        bool Disconnect();
+        bool IsConnected() const;
+        
+      public:
+        bool Send(const message<T>& msg);
+        
+      protected:
+        // Each connection has a unique socket to a remote
+        asio::ip::tcp::socket m_socket;
+        
+        // This context is shared with the whole asio instance
+        asio::io_context& m_asioContext;
+        
+        // This queue holds all messages to be sent to the remote side
+        // of this connection
+        tsqueue<message<T>> m_qMessagesOut;
+        
+        // This queue holds all messages that have been recieved from
+        // the remote side of this connection. Note it is a reference
+        // as the "owner" of this connection is expected to provide a queue
+        tsqueue<owned_message>& m_qMessagesIn;
+    };
+  }
+}
