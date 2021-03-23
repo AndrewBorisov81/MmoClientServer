fe695554acf84c2efe23dff0eb6907f75a472c9e
AndrewBorisov81 <ibobojohn81@gmail.com>
2021-03-23 11:22:35 +0200

    Fix std::scoped_lock error

--- Frameworks/net_tsqueue.h
+++ Frameworks/net_tsqueue.h
@@ -17,6 +17,7 @@ namespace olc
          // Returns and maintains item at front of Queue
          const T& front()
          {
+             //std::scoped_lock; lock(muxQueue);
              std::scoped_lock lock(muxQueue);
              return deqQueue.front();
          }
@@ -24,36 +25,36 @@ namespace olc
          // Adds an item to back of Queue
          void push_back(const T& item)
          {
-             std::scoped_lock lock(muxQueue);
-             deqQueue.emplace_back(std::move(item));
+           std::scoped_lock lock(muxQueue);
+           deqQueue.emplace_back(std::move(item));
          }
-         
+                  
          // Returns number of items in Queue
          size_t count()
          {
-             std::scoped_lock lock(muxQueue);
-             return deqQueue.size();
+           std::scoped_lock lock(muxQueue);
+           return deqQueue.size();
          }
-         
+                  
          // Clears Queue
          void clear()
          {
-             std::scoped_lock lock(muxQueue);
-             deqQueue.clear();
+           std::scoped_lock lock(muxQueue);
+           deqQueue.clear();
          }
-         
-         // Removes and returns item from front of Queue
-         T pop_front()
-         {
-             std::scoped_lock lock(muxQueue);
-             auto t = std::move(dequeQueue.front());
-             deqQueue.pop_front();
-             return t;
-         }
+                  
+        // Removes and returns item from front of Queue
+        T pop_front()
+        {
+          std::scoped_lock lock(muxQueue);
+          auto t = std::move(deqQueue.front());
+          deqQueue.pop_front();
+          return t;
+        }
          
      protected:
          std::mutex muxQueue;
          std::deque<T> deqQueue;
-     }
+     };
   }
 }
