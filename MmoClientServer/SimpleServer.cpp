df91f5272d6cd50e961ee6340c4abec7b4528229
AndrewBorisov81 <ibobojohn81@gmail.com>
2021-03-29 13:10:13 +0300

    Update

--- MmoClientServer/SimpleServer.cpp
+++ MmoClientServer/SimpleServer.cpp
@@ -6,7 +6,7 @@
 //
 
 #include <iostream>
-#include <olc_net.h>
+#include "olc_net.h"
 
 enum class CustomMsgTypes : uint32_t
 {
@@ -20,21 +20,62 @@ enum class CustomMsgTypes : uint32_t
 class CustomServer : public olc::net::server_interface<CustomMsgTypes>
 {
 public:
-    CustomServer(uint16_t nPort) : olc::net::server_interface<CustomMsgTypes>(nPort);
+    CustomServer(uint16_t nPort) : olc::net::server_interface<CustomMsgTypes>(nPort)
     {
         
     }
 protected:
-    virtual bool OnClientConnect(olc::shared_ptr<olc::net::connection<CustomMsgTypes>> client)
+    virtual bool OnClientConnect(std::shared_ptr<olc::net::connection<CustomMsgTypes>> client)
     {
         olc::net::message<CustomMsgTypes> msg;
         msg.header.id = CustomMsgTypes::ServerAccept;
         client->Send(msg);
         return true;
     }
-}
+    
+    // Called when a client appears to have disconnected
+    virtual void OnClientDisconnect(std::shared_ptr<olc::net::connection<CustomMsgTypes>> client)
+    {
+        std::cout << "Removing client [" << client->GetID() << "]\n";
+    }
+    
+    // Called when a message arrives
+    virtual void OnMessage(std::shared_ptr<olc::net::connection<CustomMsgTypes>> client, olc::net::message<CustomMsgTypes>& msg)
+    {
+        switch(msg.header.id)
+        {
+        case CustomMsgTypes::ServerPing:
+        {
+            std::cout << "[" << client->GetID() << "]: Server Ping\n";
+            
+            // Simply bounce message back to client
+            client->Send(msg);
+        }
+        break;
+                
+        case CustomMsgTypes::MessageAll:
+        {
+            std::cout << "[" << client->GetID() << "]: Message All\n";
+            
+            // Constructor a new message and send to all clients
+            olc::net::message<CustomMsgTypes> msg;
+            msg.header.id = CustomMsgTypes::ServerMessage;
+            msg << client->GetID();
+            MessageAllClients(msg, client);
+        }
+        break;
+        }
+    }
+};
 
 int main(int argc, const char * argv[]) {
+    CustomServer server(60000);
+    server.Start();
+    
+    while(1)
+    {
+        server.Update(-1, true);
+    }
     
     return 0;
 }
