3d48423623f226a3a3ee1812f74fd67927f74db0
AndrewBorisov81 <ibobojohn81@gmail.com>
2021-03-29 17:22:38 +0300

    Added while loop

--- NetClient/NetClient/SimpleClient.cpp
+++ NetClient/NetClient/SimpleClient.cpp
@@ -44,5 +44,52 @@ int main(int argc, const char * argv[]) {
     CustomClient c;
     c.Connect("127.0.0.1", 600000);
     
+    
+    bool bQuit = false;
+    while(!bQuit)
+    {
+        if (c.IsConnected())
+        {
+            if(!c.Incoming().empty())
+            {
+                auto msg = c.Incoming().pop_front().msg;
+                
+                switch (msg.header.id)
+                {
+                case CustomMsgTypes::ServerAccept:
+                {
+                    // Server has responded to a ping request
+                    std::cout << "Server Accepted Connection\n";
+                }
+                break;
+                        
+                case CustomMsgTypes::ServerPing:
+                {
+                    // Server has responded to ping request
+                    std::chrono::system_clock::time_point timeNow = std::chrono::system_clock::now();
+                    std::chrono::system_clock::time_point timeThen;
+                    msg >> timeThen;
+                    std::cout << "Ping: " << std::chrono::duration<double>(timeNow - timeThen).count() << "\n";
+                }
+                break;
+                        
+                case CustomMsgTypes::ServerMessage:
+                {
+                    // Server has responded to a ping request
+                    uint32_t clientID;
+                    msg >> clientID;
+                    std::cout << "Hello from [" << clientID << "]\n";
+                }
+                break;
+                }
+            }
+        }
+        else
+        {
+            std::cout << "Server Down\n";
+            bQuit = true;
+        }
+    }
+    
     return 0;
 }
